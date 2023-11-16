14b30ca5631be814545297578205abb5ec8fb7fa
sveste123 <sverrestenbaek@icloud.com>
2023-11-16 16:23:23 +0100

    more adjustments

--- src/components/scene.svelte
+++ src/components/scene.svelte
@@ -103,7 +103,7 @@
 
 <T.PerspectiveCamera
         makeDefault
-        position={[0, 6, 6.2]}
+        position={[0, 5.3, 8.2]}
         target={[0, 2, 0]}
         rotation={[0, 40, 0]}
         on:create={({ ref }) => {
